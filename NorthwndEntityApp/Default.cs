8a599d69dc126d05af5fbe67cc95f9e8042495c8
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2023-05-27 17:18:59 +0300

    is continued

--- NorthwndEntityApp/Default.cs
+++ NorthwndEntityApp/Default.cs
@@ -28,38 +28,51 @@ namespace NorthwndEntityApp
         {
             var productList = from x in db.Products
                               where x.Discontinued == true
-                              select new { ÜrünSırası = x.ProductID, ÜrünAdı = x.ProductName, Fiyat = x.UnitPrice + " ₺" };
+                              select new { ÜrünNumarası = x.ProductID, ÜrünAdı = x.ProductName, Fiyat = x.UnitPrice + " ₺" };
             dgv_list.DataSource = productList.ToList();
         }
 
         private void TSMI_suppliers_Click(object sender, EventArgs e)
         {
-
+            var supplierList = from x in db.Suppliers
+                               select new { TedarikçiNumarası = x.SupplierID, ŞirketAdı = x.CompanyName, KişiAdı = x.ContactName, Şehir = x.City, Ülke = x.Country };
+            dgv_list.DataSource = supplierList.ToList();
         }
 
         private void TSMI_customers_Click(object sender, EventArgs e)
         {
-
+            var customerList = from x in db.Customers
+                               select new { MüşteriNumarası = x.CustomerID, ŞirketAdı = x.CompanyName, KişiAdı = x.ContactName, Şehir = x.City, Ülke = x.Country };
+            dgv_list.DataSource= customerList.ToList();
         }
 
         private void TSMI_employees_Click(object sender, EventArgs e)
         {
-
+            var employeeList = from x in db.Employees
+                               select new { ÇalışanNumarası = x.EmployeeID, Adı = x.FirstName, Soyadı = x.LastName, DoğumTarihi = x.BirthDate, Şehir = x.City };
+            dgv_list.DataSource = employeeList.ToList();
         }
 
         private void TSMI_orders_Click(object sender, EventArgs e)
         {
-
+            var orderList = from x in db.Orders
+                             where x.Freight > 100
+                             select new {SiparişNumarası = x.OrderID, MüşteriAdı = x.Customers.ContactName, SatışYapanPersonel = x.Employees.FirstName, SatışTarihi = x.OrderDate, TedarikçiŞirket = x.Shippers.CompanyName  };
+            dgv_list.DataSource = orderList.ToList();
         }
 
         private void TSMI_regions_Click(object sender, EventArgs e)
         {
-
+            var regionList = from x in db.Region
+                             select new { BölgeSırası = x.RegionID, BölgeAdı = x.RegionDescription };
+            dgv_list.DataSource = regionList.ToList();
         }
 
         private void TSMI_shippers_Click(object sender, EventArgs e)
         {
-
+            var shipperList = from x in db.Shippers
+                              select new {NakliyatçıNumarası = x.ShipperID, NakliyeciŞirket = x.CompanyName };
+            dgv_list.DataSource = shipperList.ToList();
         }
     }
 }
